7a0a1ba55f42aac72a38dbd8ed84bc32eb0bd04a
POOJA <https://github.com/Pooja-bhaskaran/Day03.git>
2022-11-16 14:55:57 +0530

    user inputted name using Regex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imgUrl = "/assets/BridgeLabz-2.jpg";
   webUrl = "https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void{
     this.title = "Hello from Bridgelabz!!";
@@ -18,5 +19,15 @@ export class AppComponent {
   onClick($event:any){
     console.log("save button is clicked!", $event);
     window.open(this.webUrl, "_blank");
+  }
+
+  onInput($event:any){
+    console.log("Change the event Occurred!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-z A-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is incorrect!";
   }
 }
